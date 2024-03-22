6c72751607f6033f6d2b207c11a7eab02bdfff7a
Polzocr <micro.cpt.62@outlook.fr>
2024-03-22 17:31:24 +0100

    ajout photo plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by follwing the Gatsby tutorial.</p>
+      <StaticImage
+        alt="plein de text sympa"
+        src="../images/stackoverflow.jpg"
+      />
     </Layout>
   )
 }
